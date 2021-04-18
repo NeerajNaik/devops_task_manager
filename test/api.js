5b1dbe71192c924ff60dd4af3b0c42894b517b39
vatsal003 <60845745+vatsal003@users.noreply.github.com>
2021-04-18 19:10:31 +0530

    Update api.js

--- test/api.js
+++ test/api.js
@@ -23,6 +23,45 @@ describe('Tasks API', () => {
             })
         }).timeout(5000);
     })
+  
+  
+  
+  
+      describe('authentication', () => {
+        it("Check if request fails without authentication", (done) => {
+            chai.request(server)
+            .get("/tasks")
+            //.set('content-type', 'application/x-www-form-urlencoded')
+            //.send({email: 'test',password: '1234567'})
+            .end((err, response) => {
+
+                //console.log(response.body)
+                response.should.have.status(401);
+                //response.body.should.be.a('array');
+                //console.log("dd")
+                done();
+            })
+        }).timeout(5000);
+    })
+
+    describe('authentication', () => {
+        it("Check if request passes with authentication", (done) => {
+            chai.request(server)
+            .get("/tasks")
+            .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDdjMmVmODc0OTAxMTYzNjRkNWFmOTgiLCJpYXQiOjE2MTg3NTE0MDd9.iznbEk5K4M7lyh75zF8x8QBtJJuvSB09Yu6ulbHWEgU')
+            //.send({email: 'test',password: '1234567'})
+            .end((err, response) => {
+
+                //console.log(response.body)
+                response.should.have.status(200);
+                //response.body.should.be.a('array');
+                //console.log("dd")
+                done();
+            })
+        }).timeout(5000);
+    })
+  
+  
     describe('GET wrong API anytask', () => {
         it("It should NOT get all tasks", (done) => {
             chai.request(server)
